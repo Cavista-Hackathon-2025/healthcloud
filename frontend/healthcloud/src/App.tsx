f1dfeeb05a92f58a54d2ee068702232636469bb3
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2025-02-23 09:33:17 +0100

    fix: routing bug

--- frontend/healthcloud/src/App.tsx
+++ frontend/healthcloud/src/App.tsx
@@ -1,19 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import { TranscriptionRoutes } from "@/routes/transcription/TranscriptionRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReportsRoutes } from "@/routes/reports/ReportsRoutes";
 import { Toaster } from "sonner";
 import { RecordingsRoutes } from "@/routes/recordings/RecordingsRoutes";
 import { GlobalError } from "@/components/GlobalError";
+import { TranscriptionPaths } from "@/routes/transcription/TranscriptionPaths";
 import { RootLayout } from "@/layouts/RootLayout";
 
 const APP_ROUTES = [TranscriptionRoutes, ReportsRoutes, RecordingsRoutes];
 
 const router = createBrowserRouter([
     {
+        path: "/",
         element: <RootLayout />,
         errorElement: <GlobalError />,
-        children: [...APP_ROUTES.flat()],
+        children: [
+            {
+                index: true,
+                element: (
+                    <Navigate to={TranscriptionPaths.TRANSCRIBE} replace />
+                ),
+            },
+            ...APP_ROUTES.flat(),
+        ],
     },
 ]);
 
